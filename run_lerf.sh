4c4b09ef405eb3e1648fc336c8409fd7df64cfac
rui <shu1l0n9@gmail.com>
2025-07-29 22:13:31 +0800

    fix output directory path and update render command syntax in run_lerf.sh

--- run_lerf.sh
+++ run_lerf.sh
@@ -1,15 +1,15 @@
 dataset_path="data/lerf_ovs/"
 dataset_names=("ramen" "teatime" "figurines" "waldo_kitchen")
-output_dir="output/lerf"
+output_dir="output/lerf_ovs/"
 
 for dataset_name in "${dataset_names[@]}"; do
     echo "Processing dataset: $dataset_name"
     # 3dgs训练模型
     python train.py -s ${dataset_path}${dataset_name} -m $output_dir/$dataset_name --iterations 30000
     # 渲染模型
-    python render.py -m $output_dir/$dataset_name --iteration 30000
+    python render.py -m {$output_dir}{$dataset_name} --iteration 30000
     # 提取特征
-    python gaussian_feature_extractor.py -m $output_dir/$dataset_name --iteration 30000 --eval --feature_level 1
+    python gaussian_feature_extractor.py -m {$output_dir}{$dataset_name} --iteration 30000 --eval --feature_level 1
     # 渲染特征
-    python feature_map_renderer.py -m $output_dir/$dataset_name --iteration 30000 --eval --feature_level 1
+    python feature_map_renderer.py -m {$output_dir}{$dataset_name} --iteration 30000 --eval --feature_level 1
 done
